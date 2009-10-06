096e7cb7c5189c95e06214b17a92bd74aaedb250
railsbank <railsbank@abfe9e85-5fe2-4dba-a5b6-d2aaa1d5e3c2>
2009-10-06 13:05:21 +0000

    git-svn-id: http://railsbank-svn.cvsdude.com/blinki/railsmine@89 abfe9e85-5fe2-4dba-a5b6-d2aaa1d5e3c2

--- app/models/method_or_class.rb
+++ app/models/method_or_class.rb
@@ -2,7 +2,7 @@ class MethodOrClass < ActiveRecord::Base
   belongs_to :document
   acts_as_solr
 
-  def MethodOrClass.delete_all
+  def MethodOrClass.delete_everything
      MethodOrClass.find(:all).each do |mc|
          puts "Deleting record: #{mc.id}"
          mc.solr_destroy
